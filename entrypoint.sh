6f889c68c82718c11e3705d23dd55905b76a3662
POFK <maotianxiang@bao.ac.cn>
2020-05-21 23:23:38 +0800

    fix(entrypoint):

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,7 @@ if [ "$MAP_NODE_UID" != "no" ]; then
     chgrp -R dev /opt/ && chmod 770 -R /opt
     chown -R dev /home/dev && chgrp -R dev /home/dev 
     echo "**** Fix Permission finished... "
-    exec gosu dev "cd ~/.vim/plugged/YouCompleteMe && python install.py"
+    exec gosu dev bash -l -c "cd ~/.vim/plugged/YouCompleteMe && python install.py"
 fi
 
 echo "**** GOSU dev $@ ..."
